4447044367bd2bacb47c0bc4634338771415316c
Diegojadrian97 <diegojadrian97@gmail.com>
2024-02-06 15:24:38 -0300

    Backend entrega finalizada

--- Frontend/src/pages/indexPage.jsx
+++ Frontend/src/pages/indexPage.jsx
@@ -9,11 +9,10 @@ import Stack from "@mui/material/Stack";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import Navbar from "../components/navbar/navbar";
 import theme from "../utils/theme";
+
 const indexPage = () => {
   const navigate = useNavigate();
-  const handleIndexClick = () => {
-    navigate("/");
-  };
+
   const handleRegisterClick = () => {
     navigate("/register");
   };
@@ -26,62 +25,61 @@ const indexPage = () => {
   const showCart = () => {
     navigate("/cart");
   };
-  const hasCookie = () => {
-    return document.cookie
+  const hasCookie = () =>
+    document.cookie
       .split(";")
       .some((item) => item.trim().startsWith("jwtCookie="));
-  };
   return (
     <div>
       <Navbar />
       <div className="idxPage">
-        {hasCookie ? (
-          <BottomNavigation
-            showLabels
-            sx={{
-              position: "absolute",
-              justifySelf: "end",
-
-              width: "7%",
-              right: "20%",
-              backgroundColor: theme.secondaryColor,
-              borderRadius: "100px",
-            }}
-          >
-            <BottomNavigationAction
-              label="Cart"
-              icon={<ShoppingCartIcon />}
-              onClick={showCart}
-            />
-          </BottomNavigation>
-        ) : null}
         <h2 sx={{ variant: "h1" }}>Bienvenido a mi entrega final de Backend</h2>
         <Stack marginTop={3} alignItems="center" spacing={3} direction="column">
           {hasCookie() ? (
-            <Button
-              onClick={handleLogoutClick}
-              variant="contained"
-              sx={{ width: "150px" }}
-            >
-              <div>Hacer logout</div>
-            </Button>
+            <div>
+              <BottomNavigation
+                showLabels
+                sx={{
+                  position: "absolute",
+                  justifySelf: "end",
+                  width: "7%",
+                  top: "100px",
+                  right: "20%",
+                  backgroundColor: theme.secondaryColor,
+                  borderRadius: "100px",
+                }}
+              >
+                <BottomNavigationAction
+                  label="Cart"
+                  icon={<ShoppingCartIcon />}
+                  onClick={showCart}
+                />
+              </BottomNavigation>
+              <Button
+                onClick={handleLogoutClick}
+                variant="contained"
+                sx={{ width: "150px" }}
+              >
+                <div>Hacer logout</div>
+              </Button>
+            </div>
           ) : (
-            <Button
-              onClick={handleLoginClick}
-              variant="contained"
-              sx={{ width: "150px" }}
-            >
-              <div>Ir al login</div>
-            </Button>
-          )}{" "}
-          {!hasCookie() && (
-            <Button
-              onClick={handleRegisterClick}
-              variant="contained"
-              sx={{ width: "150px" }}
-            >
-              <div>Registrarse</div>
-            </Button>
+            <div>
+              <Button
+                onClick={handleRegisterClick}
+                variant="contained"
+                sx={{ width: "150px" }}
+              >
+                <div>Registrarse</div>
+              </Button>
+              <Button
+                onClick={handleLoginClick}
+                variant="contained"
+                sx={{ width: "150px", marginLeft: "10px" }}
+              >
+                <div>Ir al login</div>
+              </Button>
+            </div>
           )}
         </Stack>
       </div>
